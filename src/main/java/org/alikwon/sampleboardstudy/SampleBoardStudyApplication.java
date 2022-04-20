9d239d2dc87939e41175bb5cd3158196993a3d3a
alikwon <63032589+alikwon@users.noreply.github.com>
2022-04-20 23:32:32 +0900

    Initial commit

---
+++ src/main/java/org/alikwon/sampleboardstudy/SampleBoardStudyApplication.java
@@ -0,0 +1,13 @@
+package org.alikwon.sampleboardstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleBoardStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleBoardStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/alikwon/sampleboardstudy/SampleBoardStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alikwon.sampleboardstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleBoardStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
